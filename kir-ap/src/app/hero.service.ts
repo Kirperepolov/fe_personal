cfafd7e0d9db4157d0097aac0d10e846c64a1e5a
Kyrylo Perepolov <kyrylo.perepolov@globallogic.com>
2019-01-10 15:08:07 +0200

    HTTP services (tutorial)
    Added search method to enquire a list of heroes by a part of a name.

--- kir-ap/src/app/hero.service.ts
+++ kir-ap/src/app/hero.service.ts
@@ -86,6 +86,17 @@ export class HeroService {
       catchError(this.handleError<Hero>('deleteHero'))
     )
   }
+
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      // return empty Heroes array
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(_ => this.log(`found heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+  }
 }
 
 const httpOptions = {
